a231bc987eae2a44de12b20afe6f493c372de4bd
Ethan Young <young.ethan.scott@gmail.com>
2021-04-10 16:07:33 -0500

    remove stimulus columns from data round 3

--- blocks/run.js
+++ blocks/run.js
@@ -16,7 +16,7 @@ jsPsych.init({
     aut_data = jsPsych.data.get().ignore('aut_stim','preamble','stimulus');
     aut_browser = jsPsych.data.getInteractionData().json();
     
-    console.log(aut_data().uniqueNames());
+    console.log(aut_data.uniqueNames());
     aut_data.displayData('csv');
     
     window.parent.postMessage([aut_start, aut_data.json(), aut_browser], "https://survey.uu.nl/jfe/form/SV_bPHLspmdPfCY9ZY");
